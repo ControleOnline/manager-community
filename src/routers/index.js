bf7c963aade48b724c18e51ef5ffe5c518c1f9cc
Luiz Kim <luiz.kim@controleonline.com>
2025-04-13 20:29:06 -0300

    Update

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,18 +1,12 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import loginRoutes from '@controleonline/ui-login/src/react/router/routes';
 import managerRoutes from '@controleonline/ui-manager/src/react/router/routes';
-import ordersRoutes from '@controleonline/ui-orders/src/react/router/routes';
-import commonRoutes from '@controleonline/ui-common/src/react/router/routes';
-import peopleRoutes from '@controleonline/ui-people/src/react/router/routes';
 
 const Stack = createNativeStackNavigator();
 
 const allRoutes = [
   ...loginRoutes,
   ...managerRoutes,
-  ...ordersRoutes,
-  ...commonRoutes,
-  ...peopleRoutes,
 ];
 
 export default function Routes() {
