72bef6a0769b1ba5c22aaef737aebdd1b1b5f7d4
Luiz Kim <luiz.kim@controleonline.com>
2026-02-08 11:27:45 -0300

    Adiciona novos módulos displays e queues do subprojeto ui-ppc ao store

--- src/store/stores.js
+++ src/store/stores.js
@@ -21,6 +21,8 @@ import product_group from '@controleonline/ui-products/src/store/products/produc
 import product_group_product from '@controleonline/ui-products/src/store/products/product_group_product';
 import product_group_feedstock from '@controleonline/ui-products/src/store/products/product_group_feedstock';
 import theme from '@controleonline/ui-layout/src/store/theme';
+import displays from '@controleonline/ui-ppc/src/store/modules/displays';
+import queues from '@controleonline/ui-ppc/src/store/modules/queues';
 import address from '@controleonline/ui-people/src/store/address';
 import documents from '@controleonline/ui-people/src/store/documents';
 import phones from '@controleonline/ui-people/src/store/phones';
@@ -30,6 +32,9 @@ import invoice from '@controleonline/ui-financial/src/store/invoice';
 import paymentType from '@controleonline/ui-financial/src/store/paymentType';
 import wallet from '@controleonline/ui-financial/src/store/wallet';
 import walletPaymentType from '@controleonline/ui-financial/src/store/walletPaymentType';
+import order_products_queue from '@controleonline/ui-ppc/src/store/modules/order_products_queue';
+import display_queues from '@controleonline/ui-ppc/src/store/modules/display_queues';
+
 
 export default {
   acl,
@@ -50,6 +55,10 @@ export default {
   product_group,
   product_group_product,
   product_group_feedstock,
+  order_products_queue,
+  display_queues,
+  displays,
+  queues,
   status,
   translate,
   theme,
