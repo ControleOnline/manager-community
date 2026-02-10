209a2ba53adccb5968e45b1319fc92302b1c32c8
Luiz Kim <luiz.kim@controleonline.com>
2026-02-10 15:04:21 -0300

    Routes

--- src/routers/index.js
+++ src/routers/index.js
@@ -12,7 +12,8 @@ import commonRoutes from '@controleonline/ui-common/src/react/router/routes'
 import customersRoutes from '@controleonline/ui-customers/src/react/router/routes'
 import ordersRoutes from '@controleonline/ui-orders/src/react/router/routes'
 import peopleRoutes from '@controleonline/ui-people/src/react/router/routes'
-
+import crmRoutes from '@controleonline/ui-crm/src/react/router/routes'
+import contracts from '@controleonline/ui-contracts/src/react/router/routes'
 
 
 
@@ -25,7 +26,9 @@ const allRoutes = [
   ...commonRoutes,
   ...customersRoutes,
   ...peopleRoutes,
-  ...ordersRoutes
+  ...ordersRoutes,
+  ...crmRoutes,
+  ...contracts,
 ];
 
 
